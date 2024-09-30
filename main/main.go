2ea147b991f16eb1a67b97c9da6bdfc5105b2733
chabao18 <zhixiong.lu@qq.com>
2024-09-30 11:20:39 +0800

    feat: register service

--- main/main.go
+++ main/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"gorpc"
 	"log"
 	"net"
@@ -10,14 +9,28 @@ import (
 )
 
 func startServer(addr chan string) {
+	var foo Foo
+	if err := gorpc.Register(&foo); err != nil {
+		log.Fatal("register error:", err)
+	}
 	// pick a free port
 	l, err := net.Listen("tcp", ":0")
 	if err != nil {
 		log.Fatal("network err: ", err)
 	}
+
 	log.Println("start rpc server on ", l.Addr())
 	addr <- l.Addr().String()
 	gorpc.Accept(l)
+}
+
+type Foo int
+
+type Args struct{ Num1, Num2 int }
+
+func (f Foo) Sum(args Args, reply *int) error {
+	*reply = args.Num1 + args.Num2
+	return nil
 }
 
 func main() {
@@ -28,19 +41,21 @@ func main() {
 	defer func() { _ = client.Close() }()
 
 	time.Sleep(time.Second)
+
 	// send request & receive response
 	var wg sync.WaitGroup
-	LOOP := 1
+	LOOP := 5
 	for i := 0; i < LOOP; i++ {
 		wg.Add(1)
 		go func(i int) {
 			defer wg.Done()
-			args := fmt.Sprintf("gorpc req %d", i)
-			var reply string
+			args := &Args{Num1: i, Num2: i * i}
+
+			var reply int
 			if err := client.Call("Foo.Sum", args, &reply); err != nil {
 				log.Fatal("call Foo.Sum error:", err)
 			}
-			log.Println("reply:", reply)
+			log.Printf("%d + %d = %d", args.Num1, args.Num2, reply)
 		}(i)
 	}
 	wg.Wait()
